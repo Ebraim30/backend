08ce22b4a957dea7ec17905c2a01a6e4173d146e
Ebraim30 <ebraim.herrrera@gmail.com>
2025-02-04 15:18:11 -0300

    PITONZINHO

--- alunos/admin.py
+++ alunos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Estado
-
+from .models import Estado, Cidade, Pessoa
 
 admin.site.register(Estado)
+admin.site.register(Pessoa)
+admin.site.register(Cidade)
--- alunos/models.py
+++ alunos/models.py
@@ -7,3 +7,20 @@ class Estado(models.Model):
 
     def __str__(self):
         return f"{self.nome} ({self.sigla})"
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+
+    def __str__(self):
+        return self.nome
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=30)
+    data = models.DateField(max_length=8)
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.nome} ({self.telefone}) ({self.email}) ({self.data}) ({self.cidade})"
